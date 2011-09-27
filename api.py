0274daa87f1a4e8c0594ccae93188eed05e1230d
Raynor Vliegendhart <shinnonoir+git@gmail.com>
2011-09-27 22:10:29 +0200

    Wrote a simple FrontController

--- api.py
+++ api.py
@@ -7,7 +7,11 @@ class Heatmap:
     /heatmap?video=<video_id>&[getkey=1|0]
     """
     def GET(self, video, getkey=False):
-        return '%r %r' % (video, getkey)
+        res = {}
+        res['heatmap'] = []
+        if getkey:
+            res['key'] = None
+        return res
 
 class Echo:
     def GET(self, **kwargs):
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 #!/bin/env python
 import web
 import json
-from cgi import parse_qs
 
 from api import Heatmap, Echo
 
@@ -50,38 +49,3 @@ if __name__ == '__main__':
 else:
     web.config.debug = False
     application = app.wsgifunc()
-
-
-
-#RESPONSE_JSON = [('Content-type', 'application/json')]
-#RESPONSE_TXT = [('Content-type', 'text/plain')]
-
-
-#def application(environ, start_response):
-#    query = parse_qs(environ['QUERY_STRING'])
-#    path = parse_qs(environ['PATH_INFO'])
-#    
-#    response_headers = RESPONSE_TXT
-#    response_body = ''
-#    
-#    if path in routes:
-#        try:
-#            response_status = '200 OK'
-#            response_body = routes[path](environ)
-#            response_headers = RESPONSE_JSON
-#        except Exception as e:
-#            response_status = '500'
-#            response_body = repr(e)
-#    else:
-#        response_status = '404 NotFound'
-#        response_body = 'boo'
-    
-#    
-#    
-#    response_headers = [('Content-type', 'text/plain')]
-#    start_response(response_status, response_headers)
-#    return [response_body]
-
-    #return ['Hello world!\n'] + ['{0}={1}\n'.format(k,environ[k])
-    #                             for k in sorted(environ)] + [repr(query)]
-
