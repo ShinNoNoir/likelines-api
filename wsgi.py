f3e73c7937f1ad5cbfad289e2767a238176d8d3a
Raynor Vliegendhart <shinnonoir+git@gmail.com>
2011-09-27 11:08:31 +0200

    Parsing querystring

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
+from cgi import parse_qs
+
+
 def application(environ, start_response):
+    query = parse_qs(environ['QUERY_STRING'])
+    
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return ['Hello world!\n'] + ['{0}={1}\n'.format(k,environ[k])
-                                 for k in sorted(environ)]
+                                 for k in sorted(environ)] + [repr(query)]
 
